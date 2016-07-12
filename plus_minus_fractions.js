e5cf500c31d496974fdce41b95927ebb3da09ca8
vgvenkat <vgkrish619@gmail.com>
2016-07-12 15:37:50 -0700

    Adding problem 2

---
+++ plus_minus_fractions.js
@@ -0,0 +1,48 @@
+/*Given an array of integers, calculate which fraction of its elements are positive, which fraction of its elements are negative, and which fraction of its elements are zeroes, respectively. Print the decimal value of each fraction on a new line.
+
+Note: This challenge introduces precision problems. The test cases are scaled to six decimal places, though answers with absolute error of up to  are acceptable.
+
+Input Format
+
+The first line contains an integer, , denoting the size of the array.
+The second line contains  space-separated integers describing an array of numbers .
+
+Output Format
+
+You must print the following  lines:
+
+A decimal representing of the fraction of positive numbers in the array.
+A decimal representing of the fraction of negative numbers in the array.
+A decimal representing of the fraction of zeroes in the array.
+Sample Input
+
+6
+-4 3 -9 0 4 1
+Sample Output
+
+0.500000
+0.333333
+0.166667
+Explanation
+
+There are  positive numbers,  negative numbers, and  zero in the array.
+The respective fractions of positive numbers, negative numbers and zeroes are ,  and , respectively.*/
+function main() {
+    var n = parseInt(readLine());
+    arr = readLine().split(' ');
+    arr = arr.map(Number);
+
+    var positive = 0,
+        negative = 0,
+        zero = 0;
+    for (var iterator = 0; iterator < n; iterator ++){
+        if (arr [iterator] > 0)
+            positive ++;
+        else if (arr [iterator] < 0)
+            negative ++;
+        else if (arr [iterator] == 0)
+            zero ++
+    }
+
+    console.log((positive/n).toFixed(6) +"\n"+ (negative/n).toFixed(6) +"\n"+ (zero/n).toFixed(6));
+}
